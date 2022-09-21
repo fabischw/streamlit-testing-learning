50881624afa04f71f35a3b45c332d61d3c09cf96
fabischw <107812436+fabischw@users.noreply.github.com>
2022-09-21 14:13:30 +0200

    first test, pushing first mini data project

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 #main file for testing the streamlit framework
+
+import streamlit as st
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
